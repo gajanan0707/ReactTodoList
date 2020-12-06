1da547d44d17ea45d06f48f8de90ff4ce6a4a97f
gajanan <rajputgajanan50@gmail.com>
2020-12-06 16:06:04 +0530

    TodoList

---
+++ src/TodoItems.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+
+    this.createTasks = this.createTasks.bind(this);
+  }
+
+  createTasks(item) {
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
+  }
+
+  delete(key) {
+    this.props.delete(key);
+  }
+
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+
+    return (
+      <ul className="theList">
+        <FlipMove duration={250} easing="ease-out">
+          {listItems}
+        </FlipMove>
+      </ul>
+    );
+  }
+}
+
+export default TodoItems;
